cd7c7f5590b56135daa9afe4e671540bd2443f7f
Mark McClatchy <markmcclatchy@gmail.com>
2023-10-03 15:31:32 -0400

    add CORS middleware to app

--- api/main.py
+++ api/main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-
-# from api.v1 import accounts
+from api import auth
+from api.v1_router import V1Router
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost" "http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# @app.get("/")
-# def health_check():
-#     return {"message": "Health Check"}
-
-
-# app.include_router(accounts.router)
+app.include_router(auth.router)
+app.include_router(V1Router)
